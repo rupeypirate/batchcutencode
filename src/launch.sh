cef714c891284fbf93f8886f3960a9fc9a7c3bb4
root <ericrupprecht@gmail.com>
2018-05-01 06:12:44 -0500

    changed Launch.sh to keep script running to prevent docker exit

--- src/launch.sh
+++ src/launch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "launch.sh starting"
+
+# Launch in background
 /app/cutnotify.sh &
-/app/encodenotify.sh &
+
+# launch in forground... this must stay running of docer will stop
+/app/encodenotify.sh 
 
 echo "launch.sh exiting"
