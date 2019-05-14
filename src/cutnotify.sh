a7a21d0f635c4b4efae30ecb96e64e3d208a543d
rupeypirate <38594820+rupeypirate@users.noreply.github.com>
2019-05-13 20:35:13 -0500

    added defaultsettings override file

--- src/cutnotify.sh
+++ src/cutnotify.sh
@@ -4,13 +4,19 @@
 
 echo "CUTNOTIFY: starting"
 
-re='^[0-9]+$'
-if ! [[ $SECONDS_FRONT =~ $re ]] ; then
-        SECONDS_FRONT=5
-fi
-if ! [[ $SECONDS_END =~ $re ]] ; then
-        SECONDS_END=5
-fi
+	if [[ -f transcode/defaultsettings.txt ]]; then
+			echo "defaultsettings.txt file exists"
+	fi
+	source ./settings
+	re='^[0-9]+$'
+	if ! [[ $SECONDS_FRONT =~ $re ]] ; then
+			echo "SECONDS_FRONT did not exist, using default of 5 SECONDS"
+			SECONDS_FRONT=5
+	fi
+	if ! [[ $SECONDS_END =~ $re ]] ; then
+		echo "SECONDS_END did not exist, using default of 5 SECONDS"
+			SECONDS_END=5
+	fi
 
 inotifywait -mr -e close_write "/transcode/cut/" | while read cPATH cOPTIONS cFILE
 do
--- src/encodenotify.sh
+++ src/encodenotify.sh
@@ -4,13 +4,17 @@
 
 echo "ENCODENOTIFY: starting"
 
-if ! [[ $ENCODE_PROFILE  ]] ; then
-        ENCODE_PROFILE="Fast 1080p30"
-fi
-
 
 inotifywait -m -e close_write "/transcode/encode/" | while read cPATH cPARMS cFILE
 do
+	if [[ -f transcode/defaultsettings.txt ]]; then
+			echo "defaultsettings.txt file exists"
+	fi
+	source ./settings
+	if ! [[ $ENCODE_PROFILE  ]] ; then
+			echo "ENCODE_PROFILE did not exist, using default of Fast 1080p30"
+			ENCODE_PROFILE="Fast 1080p30"
+	fi
     echo "ENCODENOTIFY: inotify: $cPATH  $cFILE $ENCODE_PROFILE"
     # inotify has the following format in file creating 3 arguments for cut.py instad of 1
     # <path to file> triggers <filename.ext>
