e36badc0a79f9f5929890748f0a5eac8de9f4351
rupeypirate <38594820+rupeypirate@users.noreply.github.com>
2019-05-16 13:15:35 -0500

    added cut only parameter

--- src/encodenotify.sh
+++ src/encodenotify.sh
@@ -13,15 +13,22 @@ do
 	
 	# Remove Windows special characters
 	ENCODE_PROFILE=`echo "$ENCODE_PROFILE" | tr -cd "[:print:]\n"`
+	CUT_ONLY=`echo "$CUT_ONLY" | tr -cd "[:print:]\n"`
 	
-	if ! [[ $ENCODE_PROFILE  ]] ; then
-			echo "ENCODE_PROFILE did not exist, using default of Fast 1080p30"
-			ENCODE_PROFILE="Fast 1080p30"
+	if [[ $CUT_ONLY =="YES"]] ; then
+		echo "CUT_ONLY enabled, skipping encoding"
+	else
+		if ! [[ $ENCODE_PROFILE  ]] ; then
+				echo "ENCODE_PROFILE did not exist, using default of Fast 1080p30"
+				ENCODE_PROFILE="Fast 1080p30"
+		fi
+		echo "ENCODENOTIFY: inotify: $cPATH  $cFILE $ENCODE_PROFILE"
+		# inotify has the following format in file creating 3 arguments for cut.py instad of 1
+		# <path to file> triggers <filename.ext>
+		python3 encode.py "$cPATH" "$cPARMS" "$cFILE" "$ENCODE_PROFILE"  < /dev/null
 	fi
-    echo "ENCODENOTIFY: inotify: $cPATH  $cFILE $ENCODE_PROFILE"
-    # inotify has the following format in file creating 3 arguments for cut.py instad of 1
-    # <path to file> triggers <filename.ext>
-    python3 encode.py "$cPATH" "$cPARMS" "$cFILE" "$ENCODE_PROFILE"  < /dev/null
 done
 
 echo "ENCODENOTIFY: ending"
+
+CUT_ONLY="YES"
